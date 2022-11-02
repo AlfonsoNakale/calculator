1ce04c60fd2b8195c6370211d3c85ddd81669689
AlfonsoNakale <alfonso.nakale@gmail.com>
2022-11-02 22:08:31 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import { greetUser } from '$utils/greet';
-
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  // Query the elements
+  const form = document.querySelector('[fs-element="form"]');
+  if (!form) return;
+  // Listen for form submissions
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // Get the data form the calculator
+    const formData = new FormData(form);
+    const amount = formData.get('amount');
+    const interest = formData.get('interest');
+    const term = formData.get('term');
+    const extra = formData.get('extra');
+    // Perform any maths
+    // I + P * r * t
+    const total = Number(amount) * (Number(interest) / 100) * Number(term);
+    console.log([total]);
+    // Display the results
+  });
 });
